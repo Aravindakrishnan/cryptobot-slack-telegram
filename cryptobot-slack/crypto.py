79e395355b4dd3e6cae60c6dbd62584da19fd44a
Aravindakrishnan <aravindapks@gmail.com>
2021-11-03 11:34:45 +0530

    added convert_currency feature

--- cryptobot-slack/crypto.py
+++ cryptobot-slack/crypto.py
@@ -10,8 +10,12 @@ class Crypto:
 
     def get_coin(self,coin_name):
         for coin in self.coins["data"]:
-            if(coin["nameid"] == coin_name.lower() or coin["symbol"].lower() == coin_name.lower()):
+            if coin["nameid"] == coin_name.lower() or coin["symbol"].lower() == coin_name.lower():
                 return coin
         return {}
-
     
+    def convert_currency(self,usd_price,fiat_name):
+        query = f"USD_{fiat_name}"
+        url = f"https://free.currconv.com/api/v7/convert?q={query}&compact=ultra&apiKey=b9e409f547a823042d5c"
+        fiat_price = requests.get(url).json()[query]
+        return usd_price * fiat_price
