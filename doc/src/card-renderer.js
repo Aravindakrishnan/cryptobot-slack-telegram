dae05a02963907bf9c2fe73eb238728607e14210
Aravindakrishnan <aravindapks@gmail.com>
2021-11-08 10:35:44 +0530

    normalized:keys of commands object

--- doc/src/card-renderer.js
+++ doc/src/card-renderer.js
@@ -1,52 +1,52 @@
-const card_content = [
+const commands = [
     {
-        command_id : 1,
-        command_name : "/getprice",
-        command_option : "[coin_name] or [coin_symbol]",
-        command_desc : "returns the current price of the coin default : USD"
+        id : 1,
+        name : "/getprice",
+        option : "[coin_name] or [coin_symbol]",
+        desc : "returns the current price of the coin default : USD"
     },
     {
-        command_id : 2,
-        command_name : "/getprice",
-        command_option : "[coin_name] or [coin_symbol] [fiat_name]",
-        command_desc : "returns the current price of the coin in fiat"
+        id : 2,
+        name : "/getprice",
+        option : "[coin_name] or [coin_symbol] [fiat_name]",
+        desc : "returns the current price of the coin in fiat"
     },
     {
-        command_id : 3,
-        command_name : "/getdetail",
-        command_option : "[coin_name] or [coin_symbol]",
-        command_desc : "returns the complete detail about the current cryptocurrency"
+        id : 3,
+        name : "/getdetail",
+        option : "[coin_name] or [coin_symbol]",
+        desc : "returns the complete detail about the current cryptocurrency"
     },
     {
-        command_id : 4,
-        command_name : "/getdetail",
-        command_option : "[coin_name] or [coin_symbol] [fiat_name]",
-        command_desc : "returns the complete detail about the current cryptocurrency with specific fiat price"
+        id : 4,
+        name : "/getdetail",
+        option : "[coin_name] or [coin_symbol] [fiat_name]",
+        desc : "returns the complete detail about the current cryptocurrency with specific fiat price"
     },
     {
-        command_id : 5,
-        command_name : "/showgraph",
-        command_option : "[coin_name or coin_symbol]",
-        command_desc : "the candle graph snapshot of the particular coin <br> default : 1d"
+        id : 5,
+        name : "/showgraph",
+        option : "[coin_name or coin_symbol]",
+        desc : "the candle graph snapshot of the particular coin <br> default : 1d"
     },
     {
-        command_id : 5,
-        command_name : "/showgraph",
-        command_option : "[coin_name or coin_symbol] [1d,7d,1m,3m,1y,ytd,all]",
-        command_desc : " return the candle graph snapshot of the particular coin's specific timeframe"
+        id : 6,
+        name : "/showgraph",
+        option : "[coin_name or coin_symbol] [1d,7d,1m,3m,1y,ytd,all]",
+        desc : " return the candle graph snapshot of the particular coin's specific timeframe"
     }
 
 
 ]
 
-const template = card_content.map(card=> {
+const template = commands.map(command=> {
 
     return (
         `
-        <div id=usage_card${card.command_id} class="usage__content">
-        <span class="usage__command">${card.command_name}</span>
-        <span class="usage__options">${card.command_option}</span>
-        <p class="usage__desc">${card.command_desc}</p>
+        <div id=usage_card${command.id} class="usage__content">
+        <span class="usage__command">${command.name}</span>
+        <span class="usage__options">${command.option}</span>
+        <p class="usage__desc">${command.desc}</p>
         </div>
         `
     )
