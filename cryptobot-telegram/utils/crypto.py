dea2aed823eed79f6833d1edad38d23149c00c16
Aravindakrishnan <aravindapks@gmail.com>
2021-11-09 21:05:39 +0530

    added documentation for crypto.py

--- cryptobot-telegram/utils/crypto.py
+++ cryptobot-telegram/utils/crypto.py
@@ -1,3 +1,5 @@
+# Crypto module used for getting JSON of cryptocurrencies 
+
 import os
 import requests
 from pathlib import Path
@@ -5,8 +7,13 @@ from dotenv import load_dotenv
 from base64 import b64encode
 import utils.candle as candle 
 
-ENV_PATH= Path(".") / "../.env"
-load_dotenv(dotenv_path=ENV_PATH)
+# Commented it for hosting in ec2 server 
+
+# TODO: uncomment it and create a .env in /cryptobot-slack directory.
+# Paste needed API_KEYS ref : README.md
+
+# ENV_PATH= Path(".") / "../.env"
+# load_dotenv(dotenv_path=ENV_PATH)
 
 class Crypto:
 
@@ -15,21 +22,34 @@ class Crypto:
         self.coins = requests.get(url).json()
     
     def get_coins(self):
+        """
+            get_coin() method returns complete JSON of top 100 cryptocurrencies.
+        """
         return self.coins
 
     def get_coin(self,coin_name):
+        """
+            get_coin() method return the JSON of specified coin
+        """
         for coin in self.coins["data"]:
             if coin["nameid"] == coin_name.lower() or coin["symbol"].lower() == coin_name.lower():
                 return coin
         return {}
-        
+    
     def convert_currency(self,usd_price,fiat_name):
+        """
+            convert_currency() method returns the converted price from USD to some other FIAT currency.
+        """
         query = f"USD_{fiat_name.upper()}"
         url = f"https://free.currconv.com/api/v7/convert?q={query}&compact=ultra&apiKey={os.environ['CONVERT_API_KEY']}"
         fiat_price = requests.get(url).json()[query]
         return float(usd_price) * fiat_price
 
     def get_candles(self,coinname,chart_interval):
+        """
+            get_candles() method saves the specified coin's candlechart in the app directory 
+            and host it in imgbb server and returns the JSON of that hosted image.
+        """
         try:
             symbol = coinname.upper()
             candle.save_chart(symbol=symbol,interval=chart_interval)
@@ -38,6 +58,10 @@ class Crypto:
             return e
     
     def host_snapshot(self,path):
+        """
+            host_snapshot() methods hosts the image to the imgbb server and returns the
+            response as JSON.
+        """
         imgbb_url = 'https://api.imgbb.com/1/upload'
         abs_path = os.path.abspath(path)
         response = requests.post(imgbb_url, 
@@ -48,5 +72,3 @@ class Crypto:
         }
         )
         return response.json()
-
-    
