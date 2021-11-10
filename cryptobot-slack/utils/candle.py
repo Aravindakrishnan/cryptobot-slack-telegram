bb9eaeed43d90b9c2b134172336bc9d3c900c2b8
Aravindakrishnan <aravindapks@gmail.com>
2021-11-10 10:42:15 +0530

    fixed exceptions

--- cryptobot-slack/utils/candle.py
+++ cryptobot-slack/utils/candle.py
@@ -8,6 +8,10 @@ def save_chart(symbol,interval="1d"):
         save_chart() method saves the candlechart image for the specified coin 
         and also returns the dataframe for the specified coin.
     """
+
+    if(interval not in "1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M"):
+        raise Exception("Invalid interval !")
+
     url = f"https://api.binance.com/api/v1/klines?symbol={symbol}USDT&interval={interval}&limit=100"
     candles = requests.get(url).json()
 
@@ -31,9 +35,14 @@ def save_chart(symbol,interval="1d"):
         'Low' : low_c,
         'Close' : close_c
     }
+
     current_time = datetime.now().time()
     current_date = date.today().strftime("%d/%m/%Y")
     title = f"{symbol}|{current_date}|{current_time}"
-    dataframe = pd.DataFrame(raw_data).set_index("Date")
-    mpf.plot(dataframe,type="candle",style="yahoo",title=title,ylabel="Price in USDT",savefig="chart.png")    
-    return dataframe
+
+    try:
+        dataframe = pd.DataFrame(raw_data).set_index("Date")
+        mpf.plot(dataframe,type="candle",style="yahoo",title=title,ylabel="Price in USDT",savefig="chart.png")    
+        return dataframe
+    except Exception as e:
+        print(e)
--- cryptobot-slack/utils/crypto.py
+++ cryptobot-slack/utils/crypto.py
@@ -25,6 +25,9 @@ class Crypto:
         """
             get_coin() method returns complete JSON of top 100 cryptocurrencies.
         """
+        if(not self.coins):
+            raise Exception("Kindly check your network connection!")
+
         return self.coins
 
     def get_coin(self,coin_name):
@@ -42,7 +45,12 @@ class Crypto:
         """
         query = f"USD_{fiat_name.upper()}"
         url = f"https://free.currconv.com/api/v7/convert?q={query}&compact=ultra&apiKey={os.environ['CONVERT_API_KEY']}"
-        fiat_price = requests.get(url).json()[query]
+        response = requests.get(url).json()
+        
+        if(not response):
+            raise Exception("Invalid fiat symbol!")
+
+        fiat_price = response[query]       
         return float(usd_price) * fiat_price
 
     def get_candles(self,coinname,chart_interval):
@@ -62,14 +70,16 @@ class Crypto:
             host_snapshot() methods hosts the image to the imgbb server and returns the
             response as JSON.
         """
-        imgbb_url = 'https://api.imgbb.com/1/upload'
-        abs_path = os.path.abspath(path)
-        response = requests.post(imgbb_url, 
-        data = {
-            'key': os.environ["IMGBB_API_KEY"], 
-            'image':b64encode(open(abs_path, 'rb').read()),
-            'name': 'chart.png',
-        }
-        )
-        return response.json()
-
+        try:
+            imgbb_url = 'https://api.imgbb.com/1/upload'
+            abs_path = os.path.abspath(path)
+            response = requests.post(imgbb_url, 
+            data = {
+                'key': os.environ["IMGBB_API_KEY"], 
+                'image':b64encode(open(abs_path, 'rb').read()),
+                'name': 'chart.png',
+            }
+            )
+            return response.json()
+        except Exception as e:
+            print(e)
